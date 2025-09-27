246116e3ab1b162bc389a64de5ecb8ce2f62db32
Garv <garvbarthwal@gmail.com>
2025-09-27 19:15:59 +0530

    Enhanced bootloader

--- boot.asm
+++ boot.asm
@@ -1,30 +1,45 @@
-ORG 0x7C00 ;THIS TELLS NASM WHERE IN MEMORY OUR PROGRAM WILL RUN
-BITS 16 ;WE ARE WRTING IN 16-BIT CODE
+ORG 0 ;This tells NASM where in memory our program will run 
 
-start:
-    mov si,message ;ADDRESS OF THE 'message' MOVE INTO SI REGISTER
-    call print ;CALLS OUR 'print' FUNCTION 
-    jmp $ ;INFINITE LOOP, GO-TO THIS FUNCTION 
+BITS 16 ;We are writing a 16-Bit code 
 
-
-print:
-    mov bx,0 ;CLEARS BX REGISTER TO USE IT AS A COUNTER
-.loop:
-    lodsb ; LOADS THE BYTE AT ADDRESS SI INTO AL AND INCREMENT SI 
-    cmp al,0 ;COMPARE THE VALUE IN AL TO 0 (end of the string)
-    je .done  ;IF AL IS 0, THEN WE ARE AT THE END OF THE STRING AND WE ARE DONE!
-    call print_char ;OTHERWISE PRINT THE CHARACTER IN AL 
-    jmp .loop ;LOOPS TO THE NEXT CHARACTER 
-.done:
-    ret ;RETURN FROM THE PRINT FUNCTION 
+_start:
+    jmp short start
+    nop 
     
+times 33 db 0 
+start:
+jmp 0x7c0:step2
+
+step2:
+    cli ; clear interrupts 
+    mov ax,0x7c0
+    mov ds,ax
+    mov es,ax
+    mov ax,0x00
+    mov ss,ax
+    sti ;enable interrupts 
+    mov si,message ;Address of 'message' move into si register 
+    call print ;Calls our print function 
+    jmp $ ;Infinite loop,GO TO THIS instruction 
+
+    print:
+    mov bx,0; clears bx register to use it as a counter 
+
+.loop:
+    lodsb ; Loads the byte at address si into al and then increment si 
+    cmp al,0 ; Compare the value of al to 0 
+    je .done ;If al is equal to 0,then we are at the end of the string and we are done 
+    call print_char ;Otherwise print the character in al 
+    jmp .loop ;Loop to the next character 
+
+.done:
+    ret ; Return from the print statement 
+
 print_char:
-    mov ah,0eh ;MOVE 0eh INTO AH,THIS IS THE BIOS SERVICE, NUMBER FOR PRINTING THE CHARACTER 
-    int 0x10 ;CALL BIOS INTERRUPT 0x10
-    ret ;RETURN FROM print_char FUNCTION 
+    mov ah,0eh ; Move 0eh int ah,this is the bios service , number for printing the character
+    int 0x10 ; Interrupt which handle the screen operations 
+    ret ; Return from print_char function 
 
-message: db 'Hello World!',0 ;OUR null-terminated STRING TO BE PRINTED
-times 510-($ -$$) db 0; FILL THE REST OF THE SECTOR WITH ZEROS, UPTO 510 BYTES 
-dw 0xAA55 ;THE BOOT SECTOR SIGNATURE 
-
-
+message: db 'Hello world!',0 ;String to be printed terminated by 0 
+times 510-($-$$) db 0 ;Fill the rest sector with 0 upto 510 bytes
+dw 0xAA55 ;The boot sector signature
