610d055758c50cd6ae12e1075588871f27793c10
Garv <garvbarthwal@gmail.com>
2025-10-02 23:53:07 +0530

    checkpoint:Kernel_Main breakpoint working && repository cleanup

--- data.asm
+++
@@ -1,2 +0,0 @@
-message: db 'Hello from bootloader',0
-times 512 -($-$$) db 0
--- src/kernel.asm
+++ src/kernel.asm
@@ -5,22 +5,22 @@ CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
 _start:
-    mov ax,DATA_SEG
-    mov ds,ax 
-    mov es,ax 
-    mov fs,ax
-    mov gs,ax 
-    mov ss,ax 
-    mov ebp,0x00200000
-    mov esp,ebp
+    mov ax, DATA_SEG
+    mov ds, ax 
+    mov es, ax 
+    mov fs, ax
+    mov gs, ax 
+    mov ss, ax 
+    mov ebp, 0x00200000
+    mov esp, ebp
 
-    in al,0x92
-    or al,2
-    out 0x92,al
+    ; Enable A20 Line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
 
     extern kernel_main
     call kernel_main
-    jmp $
 
-times 512-($-$$) db 0
-    
+    ; Halt the CPU
+    jmp $
