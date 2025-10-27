147ccc08c021ca6ece85962fed1c49c3d9a5f98b
Abhivyakti Satti <abhivyaktisatti2005@gmail.com>
2025-10-28 01:09:05 +0530

    Added and updated FAT16 filesystem structures

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -1,6 +1,40 @@
 #include "fat16.h"
 #include "string/string.h"
 #include "status.h"
+#include "disk/disk.h"
+#include "disk/streamer.h"
+#include <stdint.h>
+
+#define OS_FAT16_SIGNATURE 0x29
+#define OS_FAT16_FAT_ENTRY_SIZE 0x02
+#define OS_FAT16_BAD_SECTOR 0xFF7
+#define OS_FAT16_UNUSED 0x00
+
+typedef unsigned int FAT_ITEM_TYPE;
+
+#define FAT_ITEM_TYPE_DIRECTORY 0
+#define FAT_ITEM_TYPE_FILE 1
+
+// FAT directory entry attribute bitmask
+#define FAT_FILE_READ_ONLY 0x01
+#define FAT_FILE_HIDDEN 0x02
+#define FAT_FILE_SYSTEM 0x04
+#define FAT_FILE_VOLUME_LABEL 0x08
+#define FAT_FILE_SUBDIRECTORY 0x10
+#define FAT_FILE_ARCHIVED 0x20
+#define FAT_FILE_DEVICE 0x40
+#define FAT_FILE_RESERVED 0x80
+
+struct fat_header_extended
+{
+    uint8_t drive_number;
+    uint8_t win_nt_bit;
+    uint8_t signature;
+    uint32_t volume_id;
+    uint8_t volume_id_string[11];
+    uint8_t system_id_string[8];
+} __attribute__((packed));
+
 
 int fat16_resolve(struct disk* disk);
 void* fat16_open(struct disk* disk, struct path_part* path, FILE_MODE mode);
