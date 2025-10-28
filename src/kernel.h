c6014c51ab9a13d081e17eabde3e7c56bf86a62a
Abhivyakti Satti <abhivyaktisatti2005@gmail.com>
2025-10-28 21:51:18 +0530

    Implemented VFS fopen() function (Chapter 70)

--- src/kernel.h
+++ src/kernel.h
@@ -5,6 +5,11 @@
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 20
 
+#define ERROR(value) (void*)(value)
+#define ERROR_I(value) (int)(value)
+#define ISERR(value) ((int)value < 0)
+
+
 void kernel_main();
 void print(const char*str);
 #endif
